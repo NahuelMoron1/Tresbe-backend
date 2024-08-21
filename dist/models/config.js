8c2e02e6e3c15f5c3073e42c3f9592daf32b89ff
moronnahuu1 <nahuelarielmoron1@gmail.com>
2024-08-21 10:58:24 -0300

    undefined en environments arreglado

--- dist/models/config.js
+++ dist/models/config.js
@@ -2,10 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DB_PORT = exports.DB_NAME = exports.DB_PASSWORD = exports.DB_USER = exports.DB_HOST = exports.PORT = void 0;
 exports.PORT = process.env.PORT || '3000';
-exports.DB_HOST = process.env.DB_HOST; //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
-exports.DB_USER = process.env.DB_USER; //|| 'admin';
-exports.DB_PASSWORD = process.env.DB_PASSWORD; //|| 'Bocajuniors10';
-exports.DB_NAME = process.env.DB_NAME; //|| 'tresbeapp';
+exports.DB_HOST = '';
+if (process.env.DB_HOST) {
+    exports.DB_HOST = process.env.DB_HOST; //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
+}
+exports.DB_USER = '';
+if (process.env.DB_USER) {
+    exports.DB_USER = process.env.DB_USER; //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
+}
+exports.DB_PASSWORD = '';
+if (process.env.DB_PASSWORD) {
+    exports.DB_PASSWORD = process.env.DB_PASSWORD; //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
+}
+exports.DB_NAME = '';
+if (process.env.DB_NAME) {
+    exports.DB_NAME = process.env.DB_NAME; //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
+}
 exports.DB_PORT = 3306;
 if (process.env.DB_PORT) {
     exports.DB_PORT = parseInt(process.env.DB_PORT); //|| 3306;
--- src/models/config.ts
+++ src/models/config.ts
@@ -1,9 +1,23 @@
 export const PORT = process.env.PORT || '3000';
+export let DB_HOST: string = '';
+if(process.env.DB_HOST){
+    DB_HOST = process.env.DB_HOST //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
+}
 
-export const DB_HOST = process.env.DB_HOST //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
-export const DB_USER = process.env.DB_USER //|| 'admin';
-export const DB_PASSWORD = process.env.DB_PASSWORD //|| 'Bocajuniors10';
-export const DB_NAME = process.env.DB_NAME //|| 'tresbeapp';
+export let DB_USER: string = '';
+if(process.env.DB_USER){
+    DB_USER = process.env.DB_USER //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
+}
+
+export let DB_PASSWORD: string = '';
+if(process.env.DB_PASSWORD){
+    DB_PASSWORD = process.env.DB_PASSWORD //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
+}
+
+export let DB_NAME: string = '';
+if(process.env.DB_NAME){
+    DB_NAME = process.env.DB_NAME //|| 'database-1.c106g4g4oxyf.us-east-2.rds.amazonaws.com';
+}
 export let DB_PORT: number = 3306;
 if(process.env.DB_PORT){
     DB_PORT = parseInt(process.env.DB_PORT) //|| 3306;
